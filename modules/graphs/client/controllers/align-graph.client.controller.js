a038995b9b0af9d196751ff589ce0233c4fbd23b
Cameron Behar <0x24a537r9@gmail.com>
2016-02-28 16:49:13 -0800

    Implement the alignment toolbar.

--- modules/graphs/client/controllers/align-graph.client.controller.js
+++ modules/graphs/client/controllers/align-graph.client.controller.js
@@ -18,20 +18,80 @@
     vm.distributeVertically = distributeVertically;
 
 
+    function align(axis) {
+      var vertices = getActionableVertices();
+      var median = getMedian(vertices);
+      var incidentEdges = {};
+      for (var i = 0; i < vertices.length; ++i) {
+        var vertex = vertices[i];
+        vertex[axis] = median[axis];
+        for (var id in vertex.edges) {
+          incidentEdges[id] = vertex.edges[id];
+        }
+      }
+
+      // Update the edges altered by previous vertex translations.
+      for (var id in incidentEdges) {
+        incidentEdges[id].update();
+      }
+    }
+
     function alignHorizontally() {
-      // TODO
+      align('y');
     }
     
     function alignVertically() {
-      // TODO
+      align('x');
     }
     
+    function distribute(axis) {
+      var vertices = getActionableVertices().sort((v0, v1) => v0[axis] - v1[axis]);
+      if (vertices.length < 3) {
+        return
+      }
+
+      var incidentEdges = {};
+      var spacing = (vertices[vertices.length - 1][axis] - vertices[0][axis]) /
+                    (vertices.length - 1);
+      for (var i = 0; i < vertices.length; ++i) {
+        var vertex = vertices[i];
+        vertex[axis] = vertices[0][axis] + i * spacing;
+        for (var id in vertex.edges) {
+          incidentEdges[id] = vertex.edges[id];
+        }
+      };
+      
+      // Update the edges altered by previous vertex translations.
+      for (var id in incidentEdges) {
+        incidentEdges[id].update();
+      }
+    }
+
     function distributeHorizontally() {
-      // TODO
+      distribute('x');
     }
-    
+
     function distributeVertically() {
-      // TODO
+      distribute('y');
+    }
+
+    function getActionableVertices() {
+      var vertices = graph.getSelectedVertices();
+      if (!vertices.length) {
+        for (var id in graph.vertices) {
+          vertices.push(graph.vertices[id]);
+        }
+      }
+      return vertices;
+    }
+
+    function getMedian(vertices) {
+      var xs = vertices.map(v => v.x).sort();
+      var ys = vertices.map(v => v.y).sort();
+      return {
+        x: xs[Math.floor(xs.length / 2)],
+        y: ys[Math.floor(ys.length / 2)],
+      };
     }
   }
 })();
