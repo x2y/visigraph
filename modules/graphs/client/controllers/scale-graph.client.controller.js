98aadfc0b68a6d385d8e4d2821c744f75a5a594e
Cameron Behar <0x24a537r9@gmail.com>
2016-02-28 17:34:58 -0800

    Implements the scale toolbar.

--- modules/graphs/client/controllers/scale-graph.client.controller.js
+++ modules/graphs/client/controllers/scale-graph.client.controller.js
@@ -8,6 +8,9 @@
   ScaleGraphController.$inject = ['$scope', '$state'];
 
 
+  var SCALE_FACTOR = 1.25;
+
+
   function ScaleGraphController($scope, $state) {
     var graph = $scope.vm.graph;
 
@@ -17,11 +20,48 @@
 
 
     function contract() {
-      // TODO
+      scale(1 / SCALE_FACTOR);
     }
     
     function expand() {
-      // TODO
+      scale(SCALE_FACTOR);
+    }
+    
+    function scale(factor) {
+      var vertices = getActionableVertices();
+      var centroid = getCentroid(vertices);
+      var incidentEdges = {};
+      for (var i = 0; i < vertices.length; ++i) {
+        var vertex = vertices[i];
+        vertex.x = centroid.x + factor * (vertex.x - centroid.x);
+        vertex.y = centroid.y + factor * (vertex.y - centroid.y);
+      
+        for (var id in vertex.edges) {
+          incidentEdges[id] = vertex.edges[id];
+        }
+      }
+
+      // Update the edges altered by previous vertex translations.
+      for (var id in incidentEdges) {
+        incidentEdges[id].update();
+      }
+    }
+
+    function getActionableVertices() {
+      var vertices = graph.getSelectedVertices();
+      if (!vertices.length) {
+        for (var id in graph.vertices) {
+          vertices.push(graph.vertices[id]);
+        }
+      }
+      return vertices;
+    }
+
+    function getCentroid(vertices) {
+      return {
+        x: vertices.reduce((sum, v) => v.x + sum, 0) / vertices.length,
+        y: vertices.reduce((sum, v) => v.y + sum, 0) / vertices.length,
+      };
     }
   }
 })();
